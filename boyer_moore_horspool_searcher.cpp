3e0eb9db94c77ec5ee6114b1415ad745b0736848
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-04-02 19:45:15 -0500

    Fixed formatting

--- boyer_moore_horspool_searcher.cpp
+++ boyer_moore_horspool_searcher.cpp
@@ -13,70 +13,61 @@ namespace fs = std::filesystem;
 
 auto boyer_moore_horspool_search(std::string_view needle,
 				 std::string_view::const_iterator haystack_begin,
-				 std::string_view::const_iterator haystack_end) {
-  if (haystack_begin != haystack_end) {
+				 std::string_view::const_iterator haystack_end)
+{
+  if (haystack_begin != haystack_end)
+  {
     return std::search(haystack_begin, haystack_end,
-		       std::boyer_moore_horspool_searcher(needle.begin(), needle.end()));
-  } else {
+		       std::boyer_moore_horspool_searcher(needle.begin(),
+							  needle.end()));
+  }
+  else
+  {
     return haystack_end;
   }
 }
 
-void print_colored(std::string_view str, std::string_view query)
+auto file_search(fs::path const& path, std::string_view needle)
 {
-  std::cout << str;
-  /*
-  auto pos = str.find(query);
-  if (pos == std::string_view::npos) {
-    std::cout << str;
-    return;
-  }
-  std::cout << str.substr(0, pos);
-  std::cout << termcolor::red << termcolor::bold
-	    << str.substr(pos, query.size()) << termcolor::reset;
-  print_colored(str.substr(pos + query.size()), query);
-  */
-}
-
-auto file_search(fs::path const& path, std::string_view needle) {
   auto absolute_path = fs::absolute(path);
   std::string filename = absolute_path.string();
-  /*
-  std::string content = read_file(filename);
-  std::string_view haystack = content;
-  */
 
-  //*
   auto mmap = mio::mmap_source(filename);
-  if (!mmap.is_open() || !mmap.is_mapped()) {
+  if (!mmap.is_open() || !mmap.is_mapped())
+  {
     return;
   }
-  const std::string_view haystack = mmap.data();  
-  //*/
+  const std::string_view haystack = mmap.data();
   const auto haystack_begin = haystack.cbegin();
   const auto haystack_end = haystack.cend();
   auto it = haystack_begin;
   const auto needle_size = needle.size();
 
-  while (it != haystack_end) {
+  while (it != haystack_end)
+  {
     it = boyer_moore_horspool_search(needle, it, haystack_end);
-    if (it != haystack_end) {
+    
+    if (it != haystack_end)
+    {
       auto newline_before = haystack.rfind('\n', it - haystack_begin);
       auto newline_after = std::find(it, haystack_end, '\n');
-      
+
       static auto previous_result = newline_before + 1;
-      if (newline_before == previous_result) {
-	it += 1;
+      if (newline_before == previous_result)
+      {
+	it++;
 	continue;
       }
       previous_result = newline_before;
 
       std::cout << path.c_str() << ":"
 		<< haystack.substr(newline_before + 1,
-				   newline_after - (haystack_begin + newline_before + 1) - 1)
+				   newline_after -
+				   (haystack_begin + newline_before + 1) - 1)
 		<< "\n";
     }
-    else {
+    else
+    {
       break;
     }
     it++;
@@ -87,10 +78,14 @@ void directory_search(fs::path const& path, std::string_view query)
 {	
   for (auto const& dir_entry : fs::directory_iterator(path))
     {
-      if (fs::is_regular_file(dir_entry)) {
-	try {
+      if (fs::is_regular_file(dir_entry))
+      {
+	try
+	{
 	  file_search(dir_entry.path(), query);
-	} catch (std::exception& e) {
+	}
+	catch (std::exception& e)
+	{
 	  continue;
 	}
       }
@@ -100,19 +95,24 @@ void directory_search(fs::path const& path, std::string_view query)
 void recursive_directory_search(fs::path const& path, std::string_view query) 
 {	
   for (auto const& dir_entry : fs::recursive_directory_iterator(path))
+  {
+    if (fs::is_regular_file(dir_entry))
     {
-      if (fs::is_regular_file(dir_entry)) {
-	try {
-	  file_search(dir_entry.path(), query);
-	} catch (std::exception& e) {
-	  continue;
-	}
+      try
+      {
+	file_search(dir_entry.path(), query);
+      }
+      catch (std::exception& e)
+      {
+	continue;
       }
     }
+  }
 }
 
-int main(int argc, char ** argv) {
-  const std::string needle = argv[1];
-  const std::string path = argv[2];
+int main(int argc, char ** argv)
+{
+  const std::string_view needle = argv[1];
+  const std::string_view path = argv[2];
   recursive_directory_search(path, needle);
 }
