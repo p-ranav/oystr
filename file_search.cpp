0f38d7c8ecf2deef45c5753d95a943332abdcec7
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-04-04 13:19:07 -0500

    Added wildcard matching for extensions

--- file_search.cpp
+++ file_search.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cctype>
+#include <cstring>
 #include <filesystem>
 #include <fstream>
 #include <functional>
@@ -19,14 +20,64 @@ auto is_binary_file(std::string_view haystack) {
   return (haystack.find('\0') != std::string_view::npos);
 }
 
+// Function that matches input str with
+// given wildcard pattern
+bool check_wildcard_pattern_match(std::string_view str,
+                                  std::string_view pattern) {
+  auto n = str.size();
+  auto m = pattern.size();
+
+  // empty pattern can only match with
+  // empty string
+  if (m == 0)
+    return (n == 0);
+
+  // lookup table for storing results of
+  // subproblems
+  bool lookup[n + 1][m + 1];
+
+  // initialize lookup table to false
+  memset(lookup, false, sizeof(lookup));
+
+  // empty pattern can match with empty string
+  lookup[0][0] = true;
+
+  // Only '*' can match with empty string
+  for (int j = 1; j <= m; j++)
+    if (pattern[j - 1] == '*')
+      lookup[0][j] = lookup[0][j - 1];
+
+  // fill the table in bottom-up fashion
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      // Two cases if we see a '*'
+      // a) We ignore ‘*’ character and move
+      //    to next  character in the pattern,
+      //     i.e., ‘*’ indicates an empty sequence.
+      // b) '*' character matches with ith
+      //     character in input
+      if (pattern[j - 1] == '*')
+        lookup[i][j] = lookup[i][j - 1] || lookup[i - 1][j];
+
+      // Current characters are considered as
+      // matching in two cases
+      // (a) current character of pattern is '?'
+      // (b) characters actually match
+      else if (pattern[j - 1] == '?' || str[i - 1] == pattern[j - 1])
+        lookup[i][j] = lookup[i - 1][j - 1];
+
+      // If characters don't match
+      else
+        lookup[i][j] = false;
+    }
+  }
+
+  return lookup[n][m];
+}
+
 auto filename_has_extension(std::string_view filename,
                             std::string_view extension) {
-  if (filename.length() >= extension.length()) {
-    return (0 == filename.compare(filename.length() - extension.length(),
-                                  extension.length(), extension));
-  } else {
-    return false;
-  }
+  return check_wildcard_pattern_match(filename, extension);
 }
 
 auto needle_search(std::string_view needle,
