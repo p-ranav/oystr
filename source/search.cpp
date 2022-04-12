12897da1c7e643a21a51749c49b6247b111893db
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-04-11 19:11:24 -0500

    Using AVX2 search for case sensitive scenarios - closes #2

--- source/search.cpp
+++ source/search.cpp
@@ -3,8 +3,75 @@
 #include <search.hpp>
 namespace fs = std::filesystem;
 
+#include <immintrin.h>
+
 namespace search
 {
+const char* find_avx2_more(const char* b, const char* e, char c)
+{
+  const char* i = b;
+
+  __m256i q = _mm256_set1_epi8(c);
+
+  for (; i + 32 < e; i += 32) {
+    __m256i x = _mm256_lddqu_si256(reinterpret_cast<const __m256i*>(i));
+    __m256i r = _mm256_cmpeq_epi8(x, q);
+    int z = _mm256_movemask_epi8(r);
+    if (z)
+      return i + __builtin_ffs(z) - 1;
+  }
+  if (i < e) {
+    if (e - b < 32) {
+      for (; i < e; ++i)
+        if (*i == c)
+          return i;
+    } else {
+      i = e - 32;
+      __m256i x = _mm256_lddqu_si256(reinterpret_cast<const __m256i*>(i));
+      __m256i r = _mm256_cmpeq_epi8(x, q);
+      int z = _mm256_movemask_epi8(r);
+      if (z)
+        return i + __builtin_ffs(z) - 1;
+    }
+  }
+  return e;
+}
+
+auto needle_search_avx2_case_sensitive(
+    std::string_view needle,
+    std::string_view::const_iterator haystack_begin,
+    std::string_view::const_iterator haystack_end)
+{
+  if (needle.empty()) {
+    return haystack_end;
+  }
+  const char c = needle[0];
+
+  // quickly find c in haystack
+  auto it = haystack_begin;
+  while (it != haystack_end) {
+    const char* ptr = find_avx2_more(it, haystack_end, c);
+    bool result = true;
+
+    const char* i = ptr;
+    for (auto& n : needle) {
+      if (n != *i) {
+        result = false;
+        break;
+      }
+      i++;
+    }
+
+    if (result) {
+      return ptr;
+    } else {
+      it = i;
+    }
+  }
+
+  return haystack_end;
+}
+
 auto is_binary_file(std::string_view haystack)
 {
   // If the haystack has NUL characters, it's likely a binary file.
@@ -91,7 +158,11 @@ auto file_search(std::string_view filename,
 
   while (it != haystack_end) {
     // Search for needle
-    it = needle_search(needle, it, haystack_end, ignore_case);
+    if (ignore_case) {
+      it = needle_search(needle, it, haystack_end, ignore_case);
+    } else {
+      it = needle_search_avx2_case_sensitive(needle, it, haystack_end);
+    }
 
     if (it != haystack_end && !print_only_file_without_matches) {
       if (!printed_file_name) {
