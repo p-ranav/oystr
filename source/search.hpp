22d40513b8375fec3f59b87c60f634bca0c7fb89
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-04-14 19:26:14 -0500

    Using unordered_set instead of set, ignoring dot files, ignoring files without extension

--- source/search.hpp
+++ source/search.hpp
@@ -7,10 +7,10 @@
 #include <fstream>
 #include <functional>
 #include <iostream>
-#include <set>
 #include <streambuf>
 #include <string>
 #include <string_view>
+#include <unordered_set>
 #include <vector>
 
 #include <avx2_memchr.hpp>
@@ -50,119 +50,120 @@ static inline bool has_suffix(const std::string_view& str,
 
 static inline bool has_one_of_suffixes(const std::string_view& str)
 {
-  static const std::set<std::string_view> suffixes {"~",
-                                                    ".dll",
-                                                    ".exe",
-                                                    ".o",
-                                                    ".so",
-                                                    ".dmg",
-                                                    ".7z",
-                                                    ".dmg",
-                                                    ".gz",
-                                                    ".iso",
-                                                    ".jar",
-                                                    ".rar",
-                                                    ".tar",
-                                                    ".zip",
-                                                    ".sql",
-                                                    ".sqlite",
-                                                    ".sys",
-                                                    ".tiff",
-                                                    ".tif",
-                                                    ".bmp",
-                                                    ".jpg",
-                                                    ".jpeg",
-                                                    ".gif",
-                                                    ".png",
-                                                    ".eps",
-                                                    ".raw",
-                                                    ".cr2",
-                                                    ".crw",
-                                                    ".pef",
-                                                    ".nef",
-                                                    ".orf",
-                                                    ".sr2",
-                                                    ".pdf",
-                                                    ".psd",
-                                                    ".ai",
-                                                    ".indd",
-                                                    ".arc",
-                                                    ".meta",
-                                                    ".pdb",
-                                                    ".pyc",
-                                                    ".Spotlight-V100",
-                                                    ".Trashes",
-                                                    "ehthumbs.db",
-                                                    "Thumbs.db",
-                                                    ".suo",
-                                                    ".user",
-                                                    ".lst",
-                                                    ".pt",
-                                                    ".pak",
-                                                    ".qml",
-                                                    ".ttf",
-                                                    ".html",
-                                                    "appveyor.yml",
-                                                    ".ply",
-                                                    ".FBX",
-                                                    ".fbx",
-                                                    ".uasset",
-                                                    ".umap",
-                                                    ".rc2.res",
-                                                    ".bin",
-                                                    ".d",
-                                                    ".gch",
-                                                    ".orig",
-                                                    ".project",
-                                                    ".workspace",
-                                                    ".idea",
-                                                    ".epf",
-                                                    "sdkconfig",
-                                                    "sdkconfig.old",
-                                                    "personal.mak",
-                                                    ".userosscache",
-                                                    ".sln.docstates",
-                                                    ".a",
-                                                    ".bin",
-                                                    ".bz2",
-                                                    ".dt.yaml",
-                                                    ".dtb",
-                                                    ".dtbo",
-                                                    ".dtb.S",
-                                                    ".dwo",
-                                                    ".elf",
-                                                    ".gcno",
-                                                    ".gz",
-                                                    ".i",
-                                                    ".ko",
-                                                    ".lex.c",
-                                                    ".ll",
-                                                    ".lst",
-                                                    ".lz4",
-                                                    ".lzma",
-                                                    ".lzo",
-                                                    ".mod",
-                                                    ".mod.c",
-                                                    ".o",
-                                                    ".patch",
-                                                    ".s",
-                                                    ".so",
-                                                    ".so.dbg",
-                                                    ".su",
-                                                    ".symtypes",
-                                                    ".symversions",
-                                                    ".tar",
-                                                    ".xz",
-                                                    ".zst",
-                                                    "extra_certificates",
-                                                    ".pem",
-                                                    ".priv",
-                                                    ".x509",
-                                                    ".genkey",
-                                                    ".kdev4",
-                                                    ".defaults",
-                                                    ".projbuild",
-                                                    ".mk"};
+  static const std::unordered_set<std::string_view> suffixes {
+      "~",
+      ".dll",
+      ".exe",
+      ".o",
+      ".so",
+      ".dmg",
+      ".7z",
+      ".dmg",
+      ".gz",
+      ".iso",
+      ".jar",
+      ".rar",
+      ".tar",
+      ".zip",
+      ".sql",
+      ".sqlite",
+      ".sys",
+      ".tiff",
+      ".tif",
+      ".bmp",
+      ".jpg",
+      ".jpeg",
+      ".gif",
+      ".png",
+      ".eps",
+      ".raw",
+      ".cr2",
+      ".crw",
+      ".pef",
+      ".nef",
+      ".orf",
+      ".sr2",
+      ".pdf",
+      ".psd",
+      ".ai",
+      ".indd",
+      ".arc",
+      ".meta",
+      ".pdb",
+      ".pyc",
+      ".Spotlight-V100",
+      ".Trashes",
+      "ehthumbs.db",
+      "Thumbs.db",
+      ".suo",
+      ".user",
+      ".lst",
+      ".pt",
+      ".pak",
+      ".qml",
+      ".ttf",
+      ".html",
+      "appveyor.yml",
+      ".ply",
+      ".FBX",
+      ".fbx",
+      ".uasset",
+      ".umap",
+      ".rc2.res",
+      ".bin",
+      ".d",
+      ".gch",
+      ".orig",
+      ".project",
+      ".workspace",
+      ".idea",
+      ".epf",
+      "sdkconfig",
+      "sdkconfig.old",
+      "personal.mak",
+      ".userosscache",
+      ".sln.docstates",
+      ".a",
+      ".bin",
+      ".bz2",
+      ".dt.yaml",
+      ".dtb",
+      ".dtbo",
+      ".dtb.S",
+      ".dwo",
+      ".elf",
+      ".gcno",
+      ".gz",
+      ".i",
+      ".ko",
+      ".lex.c",
+      ".ll",
+      ".lst",
+      ".lz4",
+      ".lzma",
+      ".lzo",
+      ".mod",
+      ".mod.c",
+      ".o",
+      ".patch",
+      ".s",
+      ".so",
+      ".so.dbg",
+      ".su",
+      ".symtypes",
+      ".symversions",
+      ".tar",
+      ".xz",
+      ".zst",
+      "extra_certificates",
+      ".pem",
+      ".priv",
+      ".x509",
+      ".genkey",
+      ".kdev4",
+      ".defaults",
+      ".projbuild",
+      ".mk"};
 
   return suffixes.find(str) != suffixes.end();
 }
@@ -183,12 +184,23 @@ std::size_t directory_search(const T&& iterator,
   for (auto const& dir_entry : iterator) {
     try {
       if (std::filesystem::is_regular_file(dir_entry)) {
-        const char* path_cstr = (const char*)dir_entry.path().c_str();
+        const auto& dir_path = dir_entry.path();
+        const char* path_cstr = (const char*)dir_path.c_str();
         std::string_view path_string {path_cstr, strlen(path_cstr)};
-        const auto file_size = std::filesystem::file_size(dir_entry.path());
-        if (dir_entry.path().filename().c_str()[0] == '.'
-            || file_size < query_size || file_size > 200 * 1024)
-        {
+
+        // Ignore dot files
+        if (dir_path.filename().c_str()[0] == '.') {
+          continue;
+        }
+
+        // Ignore files without an extension
+        if (!dir_path.has_extension()) {
+          continue;
+        }
+
+        // Ignore large files
+        const auto file_size = std::filesystem::file_size(dir_path);
+        if (file_size < query_size || file_size > 400 * 1024) {
           // Ignore files larger than 400 kB
           //
           // Only search text files in source code
@@ -210,13 +222,13 @@ std::size_t directory_search(const T&& iterator,
         // Check if file extension is in `include_extension` list
         // Check if file extension is NOT in `exclude_extension` list
         if ((include_extension.empty()
-             || filename_has_extension_from_list(dir_entry.path().extension(),
+             || filename_has_extension_from_list(dir_path.extension(),
                                                  include_extension))
             && (exclude_extension.empty()
-                || !filename_has_extension_from_list(
-                    dir_entry.path().extension(), exclude_extension)))
+                || !filename_has_extension_from_list(dir_path.extension(),
+                                                     exclude_extension)))
         {
-          count += read_file_and_search(dir_entry.path(),
+          count += read_file_and_search(dir_path,
                                         query,
                                         print_count,
                                         enforce_max_count,
@@ -225,9 +237,10 @@ std::size_t directory_search(const T&& iterator,
                                         print_only_file_without_matches);
         }
       } else {
-        const char* path_cstr = (const char*)dir_entry.path().c_str();
+        const auto& dir_path = dir_entry.path();
+        const char* path_cstr = (const char*)dir_path.c_str();
         std::string_view path_string {path_cstr, strlen(path_cstr)};
-        const auto filename = dir_entry.path().filename();
+        const auto filename = dir_path.filename();
         const char* filename_cstr = (const char*)filename.c_str();
 
         if (strlen(filename_cstr) > 0 && filename_cstr[0] == '.') {
@@ -235,42 +248,43 @@ std::size_t directory_search(const T&& iterator,
           continue;
         }
 
-        static const std::set<const char*> ignored_dirs = {".git",
-                                                           ".github",
-                                                           "build",
-                                                           "node_modules",
-                                                           ".vscode",
-                                                           ".DS_Store",
-                                                           "debugPublic",
-                                                           "DebugPublic",
-                                                           "debug",
-                                                           "Debug",
-                                                           "Release",
-                                                           "release",
-                                                           "Releases",
-                                                           "releases",
-                                                           "cmake-build-debug",
-                                                           "__pycache__",
-                                                           "Binaries",
-                                                           "Doc",
-                                                           "doc",
-                                                           "Documentation",
-                                                           "docs",
-                                                           "Docs",
-                                                           "bin",
-                                                           "Bin",
-                                                           "patches",
-                                                           "tar-install",
-                                                           "CMakeFiles",
-                                                           "install",
-                                                           "snap",
-                                                           "LICENSES",
-                                                           "img",
-                                                           "images",
-                                                           "imgs",
-                                                           // Unreal Engine
-                                                           "Plugins",
-                                                           "Content"};
+        static const std::unordered_set<const char*> ignored_dirs = {
+            ".git",
+            ".github",
+            "build",
+            "node_modules",
+            ".vscode",
+            ".DS_Store",
+            "debugPublic",
+            "DebugPublic",
+            "debug",
+            "Debug",
+            "Release",
+            "release",
+            "Releases",
+            "releases",
+            "cmake-build-debug",
+            "__pycache__",
+            "Binaries",
+            "Doc",
+            "doc",
+            "Documentation",
+            "docs",
+            "Docs",
+            "bin",
+            "Bin",
+            "patches",
+            "tar-install",
+            "CMakeFiles",
+            "install",
+            "snap",
+            "LICENSES",
+            "img",
+            "images",
+            "imgs",
+            // Unreal Engine
+            "Plugins",
+            "Content"};
 
         bool ignore = false;
         for (const auto& d : ignored_dirs) {
